c430318022d0145b4e0af9d20254842eca613028
kwon <kwon.wh02@gmail.com>
2021-10-17 16:02:20 +0900

    DONE : 1장 자바 시작 하기

---
+++ src/sec01_basicsyntx/EX01_JavaBasicStructure/JavaBasicStructure.java
@@ -0,0 +1,10 @@
+package sec01_basicsyntx.EX01_JavaBasicStructure;
+
+public class JavaBasicStructure {
+    public static void main(String[] args) {
+
+        //한줄주석 : 화면 출력 코드 ctrl+'/'
+        System.out.println("콘솔 화면에 출력");
+
+    }
+}
