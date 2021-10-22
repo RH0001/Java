ca6f4eee9b61b614aa5a78939b6f4eb73e14d211
kwon <kwon.wh02@gmail.com>
2021-10-23 00:24:12 +0900

    add : Data Type

--- src/exam01/Test.java
+++
@@ -1,7 +0,0 @@
-package exam01;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("콘솔 화면 출력");
-    }
-}
--- src/mypack/Test.java
+++
@@ -1,7 +0,0 @@
-package mypack;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("First");
-    }
-}
